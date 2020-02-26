bdd8adcdf2da3b04cebc7e1de513d8ec4b4ed0a6
lijo <lijo.abraham@imaginea.com>
2020-02-26 11:58:58 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Home v-if="!started" @start-clicked="onClickStartButton" />
+    <Question v-if="started" />
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home";
+import Question from "./components/Question";
+
+export default {
+  name: "App",
+  data: function() {
+    return {
+      started: false
+    };
+  },
+  components: {
+    Home,
+    Question
+  },
+  methods: {
+    onClickStartButton(value) {
+      this.started = value;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container fluid>
+    <v-card class="mx-auto">
+      <v-card-title
+        style="margin: 0 auto;display: block;background-color: aliceblue;"
+        class="headline grey lighten-4 text-center"
+        primary-title
+      >
+        Question
+      </v-card-title>
+      <v-card-text>
+        <p>Click below to start the Interview</p>
+      </v-card-text>
+      <v-card-actions style="margin: 0 auto;display: block;">
+        <v-btn @click="started = true" color="red lighten-2">
+          Proceed
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Question",
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
