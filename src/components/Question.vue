4be7851d2ad3129125df122c3a76cce784bfa15e
lijo <lijo.abraham@imaginea.com>
2020-03-10 14:47:42 +0530

    Vuex added

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -92,29 +92,7 @@ export default {
   },
   computed: {
     getQuestions: function() {
-      return [
-        {
-          qid: 1,
-          question: "Which directive is used to make two-way binding ?",
-          options: [
-            { oid: 1, answer: "no one" },
-            { oid: 2, answer: "v-inline" },
-            { oid: 3, answer: "v-on" },
-            { oid: 4, answer: "v-model", correct: true }
-          ]
-        },
-        {
-          qid: 2,
-          question:
-            "Which directive is to used to attach event listeners that invoke methods?",
-          options: [
-            { oid: 5, answer: "v-for" },
-            { oid: 6, answer: "v-on" },
-            { oid: 7, answer: "v-model" },
-            { oid: 8, answer: "v-bind", correct: true }
-          ]
-        }
-      ];
+      return this.$store.state.questions;
     }
   }
 };
---
+++ src/store/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    questions: [
+      {
+        qid: 1,
+        question: "Which directive is used to make two-way binding ?",
+        options: [
+          { oid: 1, answer: "no one" },
+          { oid: 2, answer: "v-inline" },
+          { oid: 3, answer: "v-on" },
+          { oid: 4, answer: "v-model", correct: true }
+        ]
+      },
+      {
+        qid: 2,
+        question:
+          "Which directive is to used to attach event listeners that invoke methods?",
+        options: [
+          { oid: 5, answer: "v-for" },
+          { oid: 6, answer: "v-on" },
+          { oid: 7, answer: "v-model" },
+          { oid: 8, answer: "v-bind", correct: true }
+        ]
+      }
+    ]
+  }
+});
+
+export default store;
